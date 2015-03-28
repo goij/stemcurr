9d8aa809f1f119669b17b5f0cc9b42b58c839e24
mmichaels01 <theomegapoint@gmail.com>
2015-03-27 19:01:08 -0500

    updated
    update

---
+++ app/Subject.php
@@ -0,0 +1,23 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class Subject extends Model {
+    use SoftDeletes;
+    /**
+     * @var array
+     */
+	protected $fillable = ['name']; 
+
+    /**
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function lessons(){
+        return $this->hasMany('App\Lesson');
+    }
+}
---
+++ database/seeds/SubjectsSeeder.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mike
+ * Date: 2/19/2015
+ * Time: 8:01 PM
+ */
+
+use Illuminate\Database\Seeder;
+use App\Subject;
+
+class SubjectsSeeder extends Seeder{
+    public function run(){
+        DB::table('subjects')->delete();
+
+        $names =[
+            'Geology & Space',
+            'Matter & Energy',
+            'Force & Motion',
+            'Weather & Climate',
+            'Structure & Function',
+            'Ecosystems & Adaptations'
+        ];
+
+        foreach($names as $name){
+            Subject::create(['name' => $name]);
+        }
+    }
+}

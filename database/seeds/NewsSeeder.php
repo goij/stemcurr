b24a6205fb22b6d70628392f3f6516482b961040
mmichaels01 <theomegapoint@gmail.com>
2015-04-13 13:39:51 -0500

    seeder
    seeder

--- database/seeds/NewsSeeder.php
+++ database/seeds/NewsSeeder.php
@@ -18,8 +18,7 @@ class NewsSeeder extends Seeder
     {
         DB::table('news')->delete();
 
-        News::create(['title'=>'Revisions','text'=>'Added assessment section in responses. Investigating file IO. Changed smiller to sjmiller in the database. Changed purple to "sea".','user_id'=>1]);
         News::create(['title'=>'Next On Agenda','text'=>'Investigating response file attachments and possible excel spreadsheet for standards. Compact standards list to expandable tabs.','user_id'=>1]);
-
+        News::create(['title'=>'Revisions','text'=>'Added assessment section in responses. Changed smiller to sjmiller in the database. Changed purple to "sea". Removed grades 1 and 2.','user_id'=>1]);
     }
 }

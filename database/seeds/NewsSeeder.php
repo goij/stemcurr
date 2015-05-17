942ec5042dfe1beb338f359860976c87c95a74e7
mmackay03 <mm.renee24@gmail.com>
2015-05-17 18:45:24 -0500

    News Seeder

--- database/seeds/NewsSeeder.php
+++ database/seeds/NewsSeeder.php
@@ -17,6 +17,7 @@ class NewsSeeder extends Seeder
     public function run()
     {
         DB::table('news')->delete();
+        News::create(['title'=>'Color Revisions','text'=>'Updated colors throughout the site to coordinate with the AU color scheme.','user_id'=>10]);
         News::create(['title'=>'Updates','text'=>'Implemented my take on everything discussed on Monday. Barring any linux issues the only remaining point from Monday should be the excels spreadsheet to ease the standard creation. File IO is implemented, as well as expandable lists for standards','user_id'=>1]);
         News::create(['title'=>'Next On Agenda','text'=>'Investigating response file attachments and possible excel spreadsheet for standards. Compact standards list to expandable tabs.','user_id'=>1]);
         News::create(['title'=>'Revisions','text'=>'Added assessment section in responses. Changed smiller to sjmiller in the database. Changed purple to "sea". Removed grades 1 and 2.','user_id'=>1]);

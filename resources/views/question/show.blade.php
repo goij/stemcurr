59f51a3f918400fa09bb5f33fc3dc456631c73bc
mmackay03 <mm.renee24@gmail.com>
2015-08-19 14:31:42 -0500

    Fix Question show

--- resources/views/question/show.blade.php
+++ resources/views/question/show.blade.php
@@ -9,8 +9,6 @@
                 <h2>Evidence of Learning</h2>
                 <p>{!!$question->evidence!!}</p>
 
-                <h2>AU Course Alignment</h2>
-                <p>{!!$question->course_align!!}</p>
 
                 <h2>Standards</h2>
                 @include('question.standards_table')
